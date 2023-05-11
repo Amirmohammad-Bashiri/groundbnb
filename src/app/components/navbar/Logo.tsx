9f2048978951a20cbf6f059a7d980556dbee7f06
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2023-05-11 19:19:07 +0330

    Implemented navbar UI

---
+++ src/app/components/navbar/Logo.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+function Logo() {
+  const router = useRouter();
+
+  return (
+    <Image
+      alt="Logo"
+      className="hidden cursor-pointer md:block"
+      height={150}
+      width={150}
+      src="/images/logo.png"
+    />
+  );
+}
+
+export default Logo;
